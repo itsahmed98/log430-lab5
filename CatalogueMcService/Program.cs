a5fafad250ff2c7e0d6277ba90d6565597801a87
itsahmed98 <ahmed.akram98@yahoo.ca>
2025-07-03 17:58:48 -0400

    Creer le microservice CatalogueMcService pour le Domaine catalogue (gestion des produits)

---
+++ CatalogueMcService/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using CatalogueMcService.Data;
+using CatalogueMcService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CatalogueDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<ICatalogueService, CatalogueService>();
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Catalogue Service API",
+        Version = "v1",
+        Description = "API pour gérer les produits"
+    });
+
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CatalogueDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Produit Service API V1");
+    });
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
